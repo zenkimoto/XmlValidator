1b1a565450b07e52fecd163b8bc43681263fdc25
Alex Yip <alexyip123@gmail.com>
2020-07-02 21:21:28 -0700

    Refactored validate method to move tokenization out

--- src/main/java/XmlValidator.java
+++ src/main/java/XmlValidator.java
@@ -5,13 +5,18 @@ import java.util.stream.Collectors;
 
 public class XmlValidator {
     public boolean validate(String xml) {
+        List<String> tags = tokenize(xml);
+
+        return tags.get(1).equals("/" + tags.get(0));
+    }
+
+    private List<String> tokenize(String xml) {
         String[] ar = xml.split(">");
         List<String> tags = Arrays.asList(ar);
-        tags = tags.stream()
+
+        return tags.stream()
                 .map(s -> s.trim())
                 .map(s -> s.replaceFirst("<", ""))
                 .collect(Collectors.toList());
-
-        return tags.get(1).equals("/" + tags.get(0));
     }
 }
