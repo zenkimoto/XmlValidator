ecf6644f78b39b9b307410ad26c9c48368891f2c
Alex Yip <alexyip123@gmail.com>
2020-07-05 06:48:34 -0700

    Updated main java with command line parameters.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Missing XML as command line parameter.\n\n");
+            System.out.println("Usage: java Main <book></book>");
+            return;
+        }
+
         XmlValidator validator = new XmlValidator();
 
-        validator.validate("    <book>   </book>");
+        //String xml = "    <book>   </book>";
+        String xml = args[0];
+
+        boolean result = validator.validate(xml);
+
+        System.out.println("The XML provided is " + (result ? "Valid" : "Invalid"));
     }
 }
